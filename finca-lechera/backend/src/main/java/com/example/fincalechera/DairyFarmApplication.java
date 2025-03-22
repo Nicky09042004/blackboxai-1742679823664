2c124d035f36427fb791608cfd3b6cf389b8d781
Nicky09042004 <Palaciosnico.2@gmail.com>
2025-03-22 16:44:11 -0500

    Add finca-lechera/backend/src/main/java/com/example/fincalechera/DairyFarmApplication.java 1742679850826

---
+++ finca-lechera/backend/src/main/java/com/example/fincalechera/DairyFarmApplication.java
@@ -0,0 +1,11 @@
+package com.example.fincalechera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyFarmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DairyFarmApplication.class, args);
+    }
+}
